3e0ec1d47ff1b69a6a2a90c12b5b8991738db1f2
AtomicScience <dmitry.greblyuk@mail.ru>
2021-03-18 22:38:12 +0300

    viewLoader now supports controllers and constructors

--- view/template.lua
+++ view/template.lua
@@ -1,6 +1,6 @@
 local GUI = require("GUI")
 local nfpager = require("umfal")("NFPager.app")
-local widgetConstructor = nfpager.utilities.widgetConstructor
+local widgetFactory = nfpager.widgets.widgetFactory
 
 local template = {}
 
@@ -16,13 +16,17 @@ end
 function template:newInstance(...)
     local view = template.constructView(self.viewName)
 
+    template.addControllers(view)
+
+    template.invokeConstructor(view, ...)
+
     return view
 end
 
 function template.constructView(viewName)
     local tableRootContainer, tableWidgets = table.unpack(nfpager.view.viewsSource[viewName].widgets)
 
-    local rootContainer = widgetConstructor.constructFromTableProperty(tableRootContainer)
+    local rootContainer = widgetFactory.constructFromTableProperty(tableRootContainer)
 
     rootContainer.viewName = viewName
     rootContainer.widgets = {}
@@ -30,13 +34,56 @@ function template.constructView(viewName)
     for i = 1, #tableWidgets do
         local tableWidget = tableWidgets[i]
 
-        local widget, widgetID = widgetConstructor.constructFromTableProperty(tableWidget)
+        local widget, widgetID = widgetFactory.constructFromTableProperty(tableWidget)
 
         rootContainer.widgets[widgetID] = widget
         rootContainer:addChild(widget)
     end
 
     return rootContainer
+end
+
+function template.addControllers(view)
+    local controllers = template.attemptToLoadModule(nfpager.view.viewsSource[view.viewName], "controllers")
+
+    if not controllers then
+        return
+    end
+
+    for widgetName, widget in pairs(view.widgets) do
+        local loadedModule = template.attemptToLoadModule(controllers, widgetName)
+
+        if loadedModule then
+            template.addControllerToWidget(widget, loadedModule)
+        end
+    end
+end
+
+
+function template.invokeConstructor(view, ...)
+    local constructor = template.attemptToLoadModule(nfpager.view.viewsSource[view.viewName], "constructor")
+
+    if not constructor then
+        return
+    end
+
+    constructor(view, ...)
+end
+
+function template.addControllerToWidget(widget, controllerTable)
+    for controllerName, controller in pairs(controllerTable) do
+        widget[controllerName] = controller
+    end
+end
+
+function template.attemptToLoadModule(rootModule, moduleName)
+    local success, loadedModule = pcall(function() return rootModule[moduleName] end)
+
+    if success then
+        return loadedModule
+    else
+        return nil
+    end
 end
 
 return template
