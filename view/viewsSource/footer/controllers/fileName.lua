97e5f9f72d0cf092ebe8fc203afac2bda065e4f3
AtomicScience <dmitry.greblyuk@mail.ru>
2021-04-11 20:25:56 +0300

    Unnecessary shortcuts lock removed

--- view/viewsSource/footer/controllers/fileName.lua
+++ view/viewsSource/footer/controllers/fileName.lua
@@ -4,8 +4,6 @@ local fileName = {}
 
 fileName.eventHandler = function(workspace, object, eventName, ...)
     if eventName == "touch" then
-        nfpager.models.shortcuts.lock()
-
         nfpager.widgets.widgetFactory.addFilesystemDialog({
             parentWorkspace  = workspace,
             onSubmitFunction = nfpager.models.file.attemptToLoadFile
